4f3340fd5307c2accd734a297380777319a9a109
du-research2 <you@example.com>
2021-05-24 05:59:13 +0000

    add shalom

--- du-research2.go
+++ du-research2.go
@@ -1,6 +1,15 @@
 package main
+
 import "github.com/ventu-io/go-shortid"
 import "fmt"
+
+
+func Shalom(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Shalom, %v. Welcome!", name)
+    return message
+}
+
 
 func main() {
     // Get a greeting message and print it.
